78045dba1d9c8358da6feb13a026ada23f666350
Emily Shaffer <nasamuffin@gmail.com>
2019-09-26 20:08:38 -0700

    gitminder: normalize repo names earlier
    to make life easier for later work, normalize the repo dirs before
    entering the for loop. (this means later we can easily marry it to
    output from git worktree list.)

--- gitminder.sh
+++ gitminder.sh
@@ -3,29 +3,26 @@
 # usage: gitminder <root-dir...>
 
 # Find all Git repos within <root-dir> (.git)
-REPOS=($(find $@ -name ".git" -type d))
+REPOS=($(find $@ -name ".git" -type d | xargs -I% readlink -f %/..))
 
 # Find all unstaged files within each
-# TODO make it work with worktreeeeeeee
 for repo in ${REPOS[@]}; do
-  # canonicalize the path of the repo, not the .git/
-  GITDIR=$(readlink -f "$repo/..")
   # count all the modified working tree state
-  SUMMARY=$(git -C "$GITDIR" status --porcelain=v1)
+  SUMMARY=$(git -C "$repo" status --porcelain=v1)
   # count how many changes to tracked files (?? denotes untracked)
   TRACK_COUNT=$(echo "$SUMMARY" | fgrep -cv "??")
   # count how many changes to untracked files
   UNTRACK_COUNT=$(echo "$SUMMARY" | fgrep -c "??")
   # get the branch name
-  BRANCH=$(git -C "$GITDIR" name-rev HEAD --name-only)
+  BRANCH=$(git -C "$repo" name-rev HEAD --name-only)
 
   # If we have uncommitted work loose, give a summary
   if [ $(expr $TRACK_COUNT + $UNTRACK_COUNT) -ne 0 ]; then
     # /path/to/repo
-    echo "$GITDIR"
+    echo "$repo"
     #   4 unstaged files in branch mybranch
     echo "  $TRACK_COUNT tracked and $UNTRACK_COUNT untracked files in $BRANCH"
     #   last commit on 4/20/2019
-    echo "  last commit was" $(git -C "$GITDIR" show -s --pretty="%ar")
+    echo "  last commit was" $(git -C "$repo" show -s --pretty="%ar")
   fi
 done
